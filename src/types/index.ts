6d1cda22d4c0a1df69fd7250eec603ecbb083635
Yaël Guilloux <yael.guilloux@gmail.com>
2022-11-11 17:18:45 +0100

    fix(build): fix build; types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import type { PinceauTheme } from './theme'
+import type { DtFunction, PinceauTheme } from './theme'
 import type { ConfigOrPaths } from './context'
 import type { ColorSchemeModes } from './css'
 
@@ -81,4 +81,27 @@ export interface ModuleHooks {
   'pinceau:options': (options?: PinceauOptions) => Promise<void> | void
 }
 
-export interface ModuleOptions extends PinceauOptions {}
+export interface ModuleOptions extends PinceauOptions {
+}
+
+declare global {
+  const $dt: DtFunction
+  const $pinceau: string
+  const __$pProps: any
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $dt: DtFunction
+    $pinceau: string
+  }
+}
+
+declare module '@nuxt/schema' {
+  interface NuxtHooks {
+    'pinceau:options': ModuleHooks['pinceau:options']
+  }
+  interface NuxtConfig {
+    pinceau?: ModuleOptions
+  }
+}
