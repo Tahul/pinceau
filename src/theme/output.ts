0ba0e611203bcb9b67e2a54a1f631531970d1711
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-22 17:28:58 +0100

    chore(stub): only stub schema if studio enabled

--- src/theme/output.ts
+++ src/theme/output.ts
@@ -7,22 +7,25 @@ import { schemaFull, tsFull, utilsFull } from './formats'
 export async function prepareOutputDir<UserOptions extends PinceauOptions = PinceauOptions>(
   {
     outputDir = join(process.cwd(), 'node_modules/.vite/pinceau'),
+    studio,
   }: UserOptions,
 ) {
   if (!existsSync(outputDir)) { await mkdir(outputDir, { recursive: true }) }
 
-  await stubOutputs(outputDir, false)
+  await stubOutputs(outputDir, false, studio)
 
   return outputDir
 }
 
-export async function stubOutputs(buildPath: string, force = false) {
+export async function stubOutputs(buildPath: string, force = false, schema = false) {
   const files = {
     'index.css': () => '/* This file is empty because no tokens has been provided or your configuration is broken. */',
     'index.ts': tsFull,
     'utils.ts': utilsFull,
-    'schema.ts': schemaFull,
   }
+
+  // Support for configuration schema
+  if (schema) { files['schema.ts'] = schemaFull }
 
   for (const [file, stubbingFunction] of Object.entries(files)) {
     const path = join(buildPath, file)
