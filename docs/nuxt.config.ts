2d03dd4085d055a1c7e1beb8bbddf5d25c709ed3
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-29 15:35:40 +0100

    chore(docs): fix build

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,11 +1,18 @@
+import { resolve } from 'pathe'
+
+// Enforce local Pinceau
+process.env.THEME_DEV_PINCEAU_PATH = resolve(__dirname, '../src/nuxt.ts')
+
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
+  alias: {
+    'pinceau/runtime': resolve(__dirname, '../src/runtime.ts'),
+    'pinceau': resolve(__dirname, '../src/index.ts'),
+  },
   pinceau: {
     followSymbolicLinks: false,
   },
   typescript: {
     includeWorkspace: true,
-  },
-  hooks: {
-  },
+  }
 })
