ee4c2b0d5ba95117e6dc5ca5c7ba8e71a84fd148
Yaël Guilloux <yael.guilloux@gmail.com>
2023-02-24 03:04:27 +0100

    chore(types): fix typings ; remove `utils` in computed styles

--- src/types/css.ts
+++ src/types/css.ts
@@ -1,17 +1,14 @@
-import type { utils } from '../runtime/utils'
 import type * as Utils from './utils'
 import type { PinceauMediaQueries, PinceauUtils } from './theme'
 import type { DefaultThemeMap } from './map'
 import type { Variants } from './variants'
 import type { NativeProperties, PropertyValue, PseudosProperties, UsableTokens } from './properties'
 
-export type ComputedStylesUtils = typeof utils
-
 export type ColorSchemeModes = 'media' | 'class'
 
 export type ComputedStyleProp<T = UsableTokens> = T | { [key in PinceauMediaQueries]?: T }
 
-export type ComputedStyleDefinition<T = UsableTokens, ComponentProps = {}> = (props: ComponentProps, utils: ComputedStylesUtils) => PropertyValue<T> | { [key in PinceauMediaQueries]?: PropertyValue<T> }
+export type ComputedStyleDefinition<T = UsableTokens, ComponentProps = {}> = (props: ComponentProps) => PropertyValue<T> | { [key in PinceauMediaQueries]?: PropertyValue<T> }
 
 export type MappedProperty<K = string, ComponentProps = {}> = PropertyValue<K> | ComputedStyleDefinition<K, ComponentProps>
 
