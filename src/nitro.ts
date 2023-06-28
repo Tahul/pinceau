09f3f4c1fa593935bc85b780e584541e37184fb6
Yaël Guilloux <yael.guilloux@gmail.com>
2023-06-28 11:42:36 +0200

    chore(deps): up deps ; fix build (ts errors in nitro)

--- src/nitro.ts
+++ src/nitro.ts
@@ -4,9 +4,9 @@ import type { NitroAppPlugin } from 'nitropack'
 import { useStorage } from '#internal/nitro'
 
 export default <NitroAppPlugin> async function (nitro) {
-  nitro.hooks.hook('render:html', async (htmlContext, { event }) => {
+  nitro.hooks.hook('render:html' as any, async (htmlContext, { event }) => {
     const theme = await useStorage().getItem('pinceau:index.css')
-    const pinceauContent = event?.pinceauContent || { theme: undefined, runtime: undefined }
+    const pinceauContent = (event as any)?.pinceauContent || { theme: undefined, runtime: undefined }
     if (!theme?.runtime) { htmlContext.head.splice(2, 0, `<style id="pinceau-runtime-hydratable">${pinceauContent.runtime}</style>`) }
     if (!theme?.theme) { htmlContext.head.splice(2, 0, `<style id="pinceau-theme">${theme}</style>`) }
   })
